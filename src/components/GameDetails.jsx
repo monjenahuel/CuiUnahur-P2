47892d0eb8adbd67656b34047758e9f799097dd7
monjenahuel <nahuel.monje@estudiantes.unahur.edu.ar>
2023-07-04 20:27:09 -0300

    gameDetails Style

--- src/components/GameDetails.jsx
+++ src/components/GameDetails.jsx
@@ -1,43 +1,86 @@
-import React from 'react';
+import React from "react";
+import { Container, Row, Col, Image, Button } from "react-bootstrap";
 
-const GameDetails = ({game,listaFavs,addToFav,delFromFav}) => {
-    console.log("listaFavs:",listaFavs)
-    
-    const {id,title,thumbnail,description,game_url,genre,platform,publisher,developer,release_date,screenshots} = game
+const GameDetails = ({ game, listaFavs, addToFav, delFromFav }) => {
+    console.log("listaFavs:", listaFavs);
 
-    const agregarJuegoActualAFavs = async () =>{
+    const {
+        id,
+        title,
+        thumbnail,
+        description,
+        game_url,
+        genre,
+        platform,
+        publisher,
+        developer,
+        release_date,
+    } = game;
+
+    const agregarJuegoActualAFavs = async () => {
         //Si el juego no está en favs lo agrega
-        if(!listaFavs.includes(id)){
+        if (!listaFavs.includes(id)) {
             addToFav(id);
-            console.log("Añadido")
-        }else{
+            console.log("Añadido");
+        } else {
             delFromFav(id);
-            console.log("Eliminado")
+            console.log("Eliminado");
         }
     };
 
-    const juegoEnFavsBool = !listaFavs.includes(id)
+    const juegoEnFavsBool = !listaFavs.includes(id);
 
-    const favButtonText = (juegoEnFavsBool) ? "Agregar a favoritos" : "Quitar de favoritos";
+    const favButtonText = juegoEnFavsBool
+        ? "Agregar a favoritos"
+        : "Quitar de favoritos";
+
+    // return (
+    //     <div className="container gameDetails">
+    //         <img src={thumbnail} alt="" width={600}/>
+    //         <h3>{title}</h3>
+    //         {description}
+    //         <h6>{genre}</h6>
+    //         <a href={game_url}></a>
+    //         <h6>{platform}</h6>
+    //         <h6>{publisher}</h6>
+    //         <h6>{developer}</h6>
+    //         <h6>{release_date}</h6>
+    //         <button className='btn btn-warning' onClick={agregarJuegoActualAFavs}>{favButtonText}</button>
+    //     </div>
+    // )
 
     return (
-        <div className="container gameDetails">
-            <h3>{id}</h3>
-            <img src={thumbnail} alt="" width={600}/>
-            <h3>{title}</h3>
-            {description}
-            <h6>{genre}</h6>
-            <a href={game_url}></a>
-            <h6>{platform}</h6>
-            <h6>{publisher}</h6>
-            <h6>{developer}</h6>
-            <h6>{release_date}</h6>
-            <button className='btn btn-warning' onClick={agregarJuegoActualAFavs}>{favButtonText}</button>
-            {/* <Button variant='warning' onClick={xd}>Agregar a favoritos</Button> */}
-        </div>
-        
-    )
-
-}
+        <Container className="gameDetails mt-4 p-4">
+            <Row>
+                <Col sm={12} md={6}>
+                    <Row>
+                        <Col xs={12}>
+                            <Image src={thumbnail} alt="" fluid className="shadowCustom rounded" />
+                        </Col>
+                        <Col xs={12} className="mb-4">
+                            <Button
+                                variant="warning"
+                                onClick={agregarJuegoActualAFavs}
+                            >
+                                {favButtonText}
+                            </Button>
+                        </Col> 
+                        <Col xs={12} className="mb-4">
+                            <h6>Genre: {genre}</h6>
+                            <h6>Platform: {platform}</h6>
+                            <h6>Publisher:{publisher}</h6>
+                            <h6>Developer: {developer}</h6>
+                            <h6>Release Date: {release_date}</h6>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col sm={12} md={6}>
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
 export default GameDetails;
